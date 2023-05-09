73ea57f9e39d1018dfc29194047727cac3d1d1d2
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-05-09 10:16:09 -0500

    @repository added

--- src/main/java/com/example/AnimeAPI/repository/AnimeRepository.java
+++ src/main/java/com/example/AnimeAPI/repository/AnimeRepository.java
@@ -2,6 +2,8 @@ package com.example.AnimeAPI.repository;
 
 import com.example.AnimeAPI.model.Anime;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AnimeRepository extends JpaRepository<Anime, Long> {
 }
