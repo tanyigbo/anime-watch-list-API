b8d6f19173ad406ad2d7cfaed439f0b9c2e956dc
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-05-09 10:13:45 -0500

    anime model created

--- src/main/java/com/example/AnimeAPI/model/Anime.java
+++ src/main/java/com/example/AnimeAPI/model/Anime.java
@@ -18,4 +18,41 @@ public class Anime {
     private String description;
 
 
+    public Anime() {
+    }
+
+    public Anime(Long id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Anime{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
