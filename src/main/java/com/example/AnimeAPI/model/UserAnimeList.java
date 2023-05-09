b88b7dd7c465ac56402a7037b6e94a7def7b10aa
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-05-09 12:10:41 -0500

    created User and Anime join table UserAnimeList

--- src/main/java/com/example/AnimeAPI/model/UserAnimeList.java
+++ src/main/java/com/example/AnimeAPI/model/UserAnimeList.java
@@ -1,8 +1,94 @@
 package com.example.AnimeAPI.model;
 
-import javax.persistence.Entity;
+import com.example.AnimeAPI.compositeKeys.AnimeListKey;
+import com.example.AnimeAPI.enums.WatchStatus;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
 
 @Entity
 public class UserAnimeList {
 
+    @EmbeddedId
+    AnimeListKey id;
+
+    @JsonIgnore
+    @ManyToOne
+    @MapsId("userId")
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @JsonIgnore
+    @ManyToOne
+    @MapsId("animeId")
+    @JoinColumn(name = "anime_id")
+    private Anime anime;
+
+    @Column
+    private int rating;
+
+    @Column
+    private WatchStatus watchStatus;
+
+    public UserAnimeList() {
+    }
+
+    public UserAnimeList(AnimeListKey id, User user, Anime anime, int rating, WatchStatus watchStatus) {
+        this.id = id;
+        this.user = user;
+        this.anime = anime;
+        this.rating = rating;
+        this.watchStatus = watchStatus;
+    }
+
+    public AnimeListKey getId() {
+        return id;
+    }
+
+    public void setId(AnimeListKey id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Anime getAnime() {
+        return anime;
+    }
+
+    public void setAnime(Anime anime) {
+        this.anime = anime;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public WatchStatus getWatchStatus() {
+        return watchStatus;
+    }
+
+    public void setWatchStatus(WatchStatus watchStatus) {
+        this.watchStatus = watchStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "UserAnimeList{" +
+                "id=" + id +
+                ", user=" + user +
+                ", anime=" + anime +
+                ", rating=" + rating +
+                ", watchStatus=" + watchStatus +
+                '}';
+    }
 }
