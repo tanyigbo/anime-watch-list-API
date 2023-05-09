c4cc61f75f512d3e33a4c3765d88a6e11b5b1b64
Trevor H <h.trevs@outlook.com>
2023-05-09 10:36:23 -0500

    Created User Service

---
+++ src/main/java/com/example/AnimeAPI/service/UserService.java
@@ -0,0 +1,15 @@
+package com.example.AnimeAPI.service;
+
+import com.example.AnimeAPI.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+}
