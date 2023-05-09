9d5c5098ce38b9f43b035a64ce40ead6a8a8e6f7
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-05-09 09:58:38 -0500

    created Genre model

---
+++ src/main/java/com/example/AnimeAPI/model/Genre.java
@@ -0,0 +1,47 @@
+package com.example.AnimeAPI.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "genres")
+public class Genre {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false)
+    String name;
+
+    @Column(nullable = false)
+    String description;
+
+    public Genre() {
+    }
+
+    public Genre(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
