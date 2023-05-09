2ad2f5e5c4fb4b5d5c593c0ee865b662ececda59
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-05-09 14:08:57 -0500

    AnimeDetail repository made

---
+++ src/main/java/com/example/AnimeAPI/repository/AnimeDetailRepository.java
@@ -0,0 +1,9 @@
+package com.example.AnimeAPI.repository;
+
+import com.example.AnimeAPI.model.AnimeDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnimeDetailRepository extends JpaRepository <AnimeDetail, Long> {
+}
