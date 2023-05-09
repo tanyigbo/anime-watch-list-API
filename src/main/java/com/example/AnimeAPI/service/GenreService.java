f09e0f0e432f1fc27b399f18043c93b8e45a3bbb
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-05-09 09:59:53 -0500

    added GenreService

---
+++ src/main/java/com/example/AnimeAPI/service/GenreService.java
@@ -0,0 +1,17 @@
+package com.example.AnimeAPI.service;
+
+import com.example.AnimeAPI.repository.GenreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GenreService {
+
+    private final GenreRepository genreRepository;
+
+    @Autowired
+    public GenreService(GenreRepository genreRepository) {
+        this.genreRepository = genreRepository;
+    }
+
+}
