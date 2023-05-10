0f60ad68d8ac1a5e3c5e33f39252bc8ed71b4871
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-05-10 11:20:02 -0500

    Cumcumber test setup

--- src/main/java/com/example/AnimeAPI/service/UserService.java
+++ src/main/java/com/example/AnimeAPI/service/UserService.java
@@ -1,15 +1,33 @@
 package com.example.AnimeAPI.service;
 
+import com.example.AnimeAPI.model.User;
 import com.example.AnimeAPI.repository.UserRepository;
+import com.example.AnimeAPI.security.JWTUtils;
+import com.example.AnimeAPI.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private AuthenticationManager authenticationManager;
+    private MyUserDetails myUserDetails;
+    private JWTUtils jwtUtils;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder, @Lazy AuthenticationManager authenticationManager, @Lazy MyUserDetails myUserDetails, JWTUtils jwtUtils) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.myUserDetails = myUserDetails;
+        this.jwtUtils = jwtUtils;
+    }
+
+    public User createUser(User userObject) {
+        return new User();
     }
 }
--- src/test/java/definitions/SpringBootTestDefinitions.java
+++ src/test/java/definitions/SpringBootTestDefinitions.java
@@ -1,16 +1,33 @@
 package definitions;
 
+import com.example.AnimeAPI.AnimeApiApplication;
 import com.example.AnimeAPI.model.User;
 import io.cucumber.java.en.Given;
 import io.cucumber.spring.CucumberContextConfiguration;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 @CucumberContextConfiguration
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = User.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = AnimeApiApplication.class)
 public class SpringBootTestDefinitions {
+
+    private static final String BASE_URL = "http://localhost:";
+    private static final RequestSpecification request = RestAssured.given();
+    private static Response response;
+
+    @LocalServerPort
+    String port;
+
+    public SpringBootTestDefinitions() {
+        RestAssured.baseURI = BASE_URL;
+    }
 
     @Given("A user information is not registered")
     public void aUserInformationIsNotRegistered() {
+
     }
 
 }
