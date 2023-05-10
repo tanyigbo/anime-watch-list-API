f671f7139dfd5f73f1ee34f5864c4154b09779f9
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-05-10 14:28:41 -0500

    added doc string for getAllUsers method

--- src/main/java/com/example/AnimeAPI/controller/UserController.java
+++ src/main/java/com/example/AnimeAPI/controller/UserController.java
@@ -5,6 +5,7 @@ import com.example.AnimeAPI.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +26,14 @@ public class UserController {
         this.userService = userService;
     }
 
+    /**
+     * A Get request for all users
+     * ResponseEntity is made up of an HTTP Status and a response object
+     * The response object is composed of a status string a List of all User objects
+     * @return ResponseEntity
+     */
     @GetMapping(path = "/users")
+    // http://localhost:{portNumber}/api/auth/users
     public ResponseEntity<?> getAllUsers(){
         response = new HashMap<>();
         List<User> users = userService.getAllUsers();
@@ -33,6 +41,4 @@ public class UserController {
         response.put("data", users);
         return new ResponseEntity<>(response, HttpStatus.FOUND);
     }
-
-
 }
