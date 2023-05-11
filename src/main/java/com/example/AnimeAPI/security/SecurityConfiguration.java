25b25f90beceefbbca13debebf59fb131bcc11cb
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-05-11 08:50:21 -0500

    added authentication manager bean

--- src/main/java/com/example/AnimeAPI/security/SecurityConfiguration.java
+++ src/main/java/com/example/AnimeAPI/security/SecurityConfiguration.java
@@ -2,6 +2,8 @@ package com.example.AnimeAPI.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,6 +17,11 @@ public class SecurityConfiguration {
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 
     @Bean
