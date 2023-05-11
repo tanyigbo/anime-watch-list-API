a82ec4f125703e5508188c1c69f00e81a0a971d9
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-05-11 08:33:39 -0500

    added password encoding to user class

--- src/main/java/com/example/AnimeAPI/service/UserService.java
+++ src/main/java/com/example/AnimeAPI/service/UserService.java
@@ -4,6 +4,7 @@ import com.example.AnimeAPI.exception.InformationExistException;
 import com.example.AnimeAPI.model.User;
 import com.example.AnimeAPI.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,21 +12,25 @@ import java.util.Optional;
 
 @Service
 public class UserService {
+
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
     public User createUser(User userObject) {
         Optional<User> user = userRepository.findUserByUsername(userObject.getUsername());
-        if(user.isEmpty()){
-           return userRepository.save(userObject);
+        if (user.isEmpty()) {
+            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
+            return userRepository.save(userObject);
         }
         throw new InformationExistException("User with username " + userObject.getUsername() + " already exists.");
     }
