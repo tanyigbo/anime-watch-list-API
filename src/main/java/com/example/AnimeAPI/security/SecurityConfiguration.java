d73bc02ad5e3a4672a960882ebe1d7ade89cbf9d
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-05-11 15:17:05 -0500

    limited public endpoints to user register and login

--- src/main/java/com/example/AnimeAPI/security/SecurityConfiguration.java
+++ src/main/java/com/example/AnimeAPI/security/SecurityConfiguration.java
@@ -47,7 +47,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeRequests().antMatchers(
-                "/auth/users/register", "/auth/users/login", "/auth/users")
+                "/auth/users/register", "/auth/users/login")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement()
