72f2739f3ffd59ae8463500e6eb022264776c1e9
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-05-11 08:59:56 -0500

    added bean for MyUserDetaials

--- src/main/java/com/example/AnimeAPI/security/SecurityConfiguration.java
+++ src/main/java/com/example/AnimeAPI/security/SecurityConfiguration.java
@@ -2,13 +2,17 @@ package com.example.AnimeAPI.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.context.WebApplicationContext;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -34,5 +38,11 @@ public class SecurityConfiguration {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().csrf().disable();
         return http.build();
+    }
+
+    @Bean
+    @Scope(value = WebApplicationContext.SCOPE_REQUEST,proxyMode = ScopedProxyMode.TARGET_CLASS)
+    public MyUserDetails myUserDetails(){
+        return (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 }
