4d86a96fd763e28c04575b7fe94c4da1191e2b7a
Trevor H <h.trevs@outlook.com>
2023-05-10 21:03:09 -0500

    Add business logic to update anime record

--- src/main/java/com/example/AnimeAPI/service/AnimeService.java
+++ src/main/java/com/example/AnimeAPI/service/AnimeService.java
@@ -1,5 +1,6 @@
 package com.example.AnimeAPI.service;
 
+import com.example.AnimeAPI.exception.InformationExistException;
 import com.example.AnimeAPI.exception.InformationNotFoundException;
 import com.example.AnimeAPI.model.Anime;
 import com.example.AnimeAPI.repository.AnimeRepository;
@@ -44,6 +45,21 @@ public class AnimeService {
         if (anime.isPresent()) {
             animeRepository.delete(anime.get());
             return anime.get();
+        } else {
+            throw new InformationNotFoundException("Anime with given id " + animeId + " does not exist.");
+        }
+    }
+
+    public Anime updateAnime(Long animeId, Anime animeObject) {
+        Optional<Anime> anime = animeRepository.findById(animeId);
+        if (anime.isPresent()) {
+            if (animeObject.getTitle().equals(anime.get().getTitle())) {
+                throw new InformationExistException("This title " + animeObject.getTitle() + " is already in use.");
+            } else {
+                anime.get().setTitle(animeObject.getTitle());
+                anime.get().setDescription(animeObject.getDescription());
+                return animeRepository.save(anime.get());
+            }
         } else {
             throw new InformationNotFoundException("Anime with given id " + animeId + " does not exist.");
         }
