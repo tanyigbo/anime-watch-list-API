6f88d1293c604918d4c1897e198e1f232af001ac
Trevor H <h.trevs@outlook.com>
2023-05-12 22:35:16 -0500

    Remove dangling code

--- src/main/java/com/example/AnimeAPI/service/UserAnimeService.java
+++ src/main/java/com/example/AnimeAPI/service/UserAnimeService.java
@@ -4,12 +4,9 @@ import com.example.AnimeAPI.exception.InformationExistException;
 import com.example.AnimeAPI.exception.InformationNotAcceptedException;
 import com.example.AnimeAPI.exception.InformationNotFoundException;
 import com.example.AnimeAPI.model.Anime;
-import com.example.AnimeAPI.model.User;
 import com.example.AnimeAPI.model.UserAnime;
 import com.example.AnimeAPI.repository.UserAnimeRepository;
-import com.example.AnimeAPI.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -89,11 +86,6 @@ public class UserAnimeService {
         return userAnimeRepository.save(userAnime);
     }
 
-    public static User getCurrentLoggedInUser() {
-        MyUserDetails userDetails = (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return userDetails.getUser();
-    }
-
     /**
      * Takes in a long anime id and user anime object and tries to find
      * an existing record connection between the logged-in user and given
@@ -109,7 +101,7 @@ public class UserAnimeService {
      */
     public UserAnime updateAnimeInUserWatchlist(Long animeId, UserAnime userAnimeObj) {
         Anime anime = animeService.getAnimeById(animeId);
-        UserAnime userAnime = userAnimeRepository.findByUserAndAnime(getCurrentLoggedInUser(), anime);
+        UserAnime userAnime = userAnimeRepository.findByUserAndAnime(AnimeService.getCurrentLoggedInUser(), anime);
         if (userAnime != null) {
             // checks the rating before set
             userAnime.setRating(checkRating(userAnimeObj.getRating()));
